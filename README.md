5ba03afd5f6f28bb477d785aeae8ce2ae9465dc7
jkuzniarek <julienkuzniarek@gmail.com>
2020-03-07 10:46:47 -0500

    began converting HTON to a subset of EON. Completed initial working version of lexer

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # hton
-**HyperText Object Notation**
-HTON is licensed under the _MIT License_ as specified in this repository's LICENSE file, so feel free to distribute, use, and modify HTON as you see fit.
+##Disclaimer: this project is a work in progress
 
-This project was born out of my frustration with how unnecessarily verbose HTML is and inspired by the relative simplicity of JSON.
+**HyperText Object Notation (HTON)**
 
-In most use cases HTML is nested in the same non-overlapping way that JSON is. 
-So why not eliminate the redundant and unnecessary end tags and replace the tags themselves with the much cleaner and more concise curly brackets like in JSON?
-Using a simple combination of curly brackets and colons, the hton.js script will convert HTON into the HTML necessary for the browser.
+This project was born out of my frustration with how unnecessarily verbose HTML is. 
+In almost all use cases HTML is nested in the same non-overlapping way that JSON is. 
+So why not eliminate the redundant and unnecessary end tags and replace them with something much cleaner and more concise?
+Using a simpler combination of angle and square brackets, the hton.js script will convert HTON into HTML for the browser.
+
+HTON is a subset of EON, an Executable Object Notation that I have been designing, but not yet built a parser for.
+Consequently, this project will be a stepping stone in the development of that project. 
 
 HTON:
-<pre><code>{:class="row":
-    {:class="col-xs-12": 
-        {h1::Hello World!}
-    }
-}</code></pre>
+`<div class: "row"
+    <div class: "col-xs-12" [
+        <h1 "Hello World!">
+        <p "Hello Sam!">
+    ]>
+>`
 
 ## Implementation
-Implementation is as simple as including the hton.js script in a script tag somewhere in your HTML file and placing your HTON into an html tag with the class="HTON".
+Implementation is as simple as including the hton.js script in a script tag at the end of your HTML file.
 
-For a description of the HTON syntax, as well as a more substantial example, view the sample.html file in both the browser and a text editor.
+For a more precise description of the HTON syntax, as well as a more substantial example, view the sample.hton.html file written in HTON.
 
-**Note:**
-Like wine and cheese, HTON pairs best with the Bootstrap frameworks as a result of the selection of the div tag as the default tag when no others are specified.
+**License:**
+HTON is licensed under the _MIT License_ as specified in this repository's LICENSE file, so feel free to distribute, use, and modify HTON as you see fit.
--- hton.js
+++ hton.js
@@ -1,114 +1,294 @@
 // hton.js
-window.onload = function() {
-  function toHTML(input){
-    let output = "";
-    let inContents = true;
-    let inTagName = false;
-    let tagName = "";
-    let inAttributes = false;
-    let inQuote = false;
-    let quoteType = 0;
-    let currentChar = "";
-    let doubleBracket = false;
-    let tagHeap = [];
 
-    for (let i = 0; i < input.length; i++){
-      if(doubleBracket == true){
-        doubleBracket = false;
-      }else{
-        switch(input[i]){
 
-          case "{":
-            if(inContents == true && input[i+1] == "{"){
-              currentChar = "{";
-              doubleBracket = true;
-            }else{
-              currentChar = "<";
-              inTagName = true;
-              inContents = false;
-            }
-            break;
+// tag object constructor
+function tag(){
+  this.type = ''
+  this.head = []
+  this.body = []
+}
 
-          case "}":
-            if(inContents == true && input[i+1] == "}"){
-              currentChar = "}";
-              doubleBracket = true;
-            }else{
-              output += "</"+tagHeap.pop();
-              currentChar = ">";
-            }
-            break;
+// determines whether a char is a valid html attribute character
+function isAttrChar(char){
+  if(
+    (char >= 'a' && char <= 'z') ||
+    (char >= 'A' && char <= 'Z') ||
+    char == '-'
+  ){ return true} else{ return false}
+}
 
-          case ":":
-            if(inTagName == true){
-              if(tagName == ""){
-                tagName = "div";
-              }
-              output += tagName;
-              tagHeap.push(tagName);
-              tagName = "";
-              inTagName = false;
-              inAttributes = true;
-              currentChar = " ";
-            }
-            else if(inAttributes == true && inQuote == false){
-              currentChar = ">";
-              inContents = true;
-              inAttributes = false;
-            }
-            else if(inContents == true || inQuote == true){
-              currentChar = input[i];
-            }
-            break;
+function isWS(char){
+  let out = false
+  switch(char){
+    case ' ': case '\n': case '\t': case '\r':
+        out = true
+  }
+  return out
+}
 
-          case "'": // quoteType 1
-            if(inAttributes == true){
-              if(inQuote == false){
-                inQuote = true;
-                quoteType = 1;
-              }
-              else if(inQuote == true && quoteType == 1){
-                inQuote = false;
-                quoteType = 0;
-              }
-            }
-            currentChar = "'";
-            break;
-            
-          case '"': // quoteType 2
-            if(inAttributes == true){
-              if(inQuote == false){
-                inQuote = true;
-                quoteType = 2;
-              }
-              else if(inQuote == true && quoteType == 2){
-                inQuote = false;
-                quoteType = 0;
-              }
-            }
-            currentChar = '"';
-            break; 
+// gets the index of the next occurrence of the input string from the input start
+// str.indexOf(searchValue [, fromIndex])
+// gets the string from start index up to but not including end index
+// str.slice(startIncluded, endExcluded)
 
-          default:
-            if(inTagName == true && input[i] != " "){
-              tagName += input[i];
-            }
-            else if(inTagName == true && input[i] == " "){
-              currentChar = "";
-            }else{
-              currentChar = input[i];
-            }
-            break;
-        }
-        output += currentChar;
-        currentChar = "";
-      }
+
+function lex(src){
+  
+  function lexString(sChar){
+    endI = src.indexOf(sChar, i+1)
+    if(endI == -1){
+      lexeme += src.slice(i+1)
+      lexemes.push(lexeme)
+      lexeme = ''
+      i = src_len
     }
-    return output;
+    else if(src[endI+1] == sChar){
+      lexeme += src.slice(i+1, endI+1)
+      i = endI+1
+      lexString(sChar)
+    }
+    else{
+      lexeme += src.slice(i+1, endI)
+      lexemes.push(lexeme)
+      lexeme = ''
+      i = endI+1
+    }
+    endI = null
   }
-  let list = document.getElementsByClassName("HTON");
-  for (let element of list){
-    let raw = String(element.innerHTML);
-    element.innerHTML = toHTML(raw);
+
+  let lexemes = []
+  let lexeme = ''
+  let endI
+
+  const src_len = src.length
+  let i = 0
+
+  while (0 <= i && i < src_len){
+    switch(src[i]){
+
+      // whitespace
+      case ' ': case '\n': case '\t': case '\r':
+        if(lexeme != ''){
+          lexemes.push(lexeme)
+          lexeme = ''
+        }
+        i++
+        break
+    
+      // comment
+      case '/':
+        if(src[i+1] == '/'){
+          endI = src.indexOf('\n', i)
+          if(endI == -1){
+            lexemes.push(src.slice(i))
+            i = src_len
+          }else{
+            lexemes.push(src.slice(i, endI))
+            i = endI
+          }
+          endI = null
+          break
+        }
+        if(src[i+1] == '*'){
+          endI = src.indexOf('*/', i+2)
+          if(endI == -1){
+            lexemes.push(src.slice(i))
+            i = src_len
+          }else{
+            lexemes.push(src.slice(i, endI+2))
+            i = endI+2
+          }
+          endI = null
+          break
+        }
+    
+      // string
+      case '"':
+        endI = ''
+        lexString('"')
+        endI = null
+        break
+      
+      case "'":
+        endI = ''
+        lexString("'")
+        endI = null
+        break
+        
+      case '`':
+        endI = ''
+        lexString('`')
+        endI = null
+        break
+        
+      case '<':
+        lexemes.push('<')
+        i++
+        break
+        
+      case '>':
+        lexemes.push('>')
+        i++
+        break
+        
+      case '[':
+        lexemes.push('[')
+        i++
+        break
+        
+      case ']':
+        lexemes.push(']')
+        i++
+        break
+        
+      case ':':
+        lexemes.push(':')
+        i++
+        break
+
+      default:
+        // type
+        if(i !== 0 && src[i-1] == '<'){
+          let indices = {}
+          indices[' '] = src.indexOf(' ', i)
+          indices['>'] = src.indexOf('>', i)
+          indices['\n'] = src.indexOf('\n', i)
+          indices['\t'] = src.indexOf('\t', i)
+          indices['\r'] = src.indexOf('\r', i)
+          endI = indices[' ']
+          let index
+          for(index in indices){
+            if(indices[index] != -1 && indices[index] < endI){
+              endI = indices[index]
+            }
+          }
+          lexeme += src.slice(i, endI)
+          for(var c of lexeme){
+            if(!isAttrChar(c)){
+              console.log("Unexpected character: '"+src[i]+"' @ index: "+i+", in: <"+lexeme)
+              i = src_len
+              break
+            }
+          }
+          if(i !== src_len){
+            lexemes.push(lexeme)
+            lexeme = ''
+            i = endI
+          }
+          endI = null
+          break
+        }
+
+        // attr
+        if(i !== 0 && src[i-1] == ' ' && isAttrChar(src[i])){
+          let indices = {}
+          indices[' '] = src.indexOf(' ', i)
+          indices[':'] = src.indexOf(':', i)
+          indices['\n'] = src.indexOf('\n', i)
+          indices['\t'] = src.indexOf('\t', i)
+          indices['\r'] = src.indexOf('\r', i)
+          endI = indices[' ']
+          let index
+          for(index in indices){
+            if(indices[index] != -1 && indices[index] < endI){
+              endI = indices[index]
+            }
+          }
+          lexeme += src.slice(i, endI)
+          for(var c of lexeme){
+            if(!isAttrChar(c)){
+              console.log("Unexpected character: '"+src[i]+"' @ index: "+i+", in: <"+lexeme)
+              i = src_len
+              break
+            }
+          }
+          if(i !== src_len){
+            lexemes.push(lexeme)
+            lexeme = ''
+            i = endI
+          }
+          endI = null
+          break
+        }
+
+      // END SWITCH CASES
+    } // END SWITCH
+    // alert(lexemes)
+  } // END WHILE
+  return lexemes;
+}
+
+
+/*
+// test
+<
+html
+lang
+:
+en
+[
+<
+head
+>
+<
+body
+Hello World
+>
+]
+>
+*/
+
+function parse(src, i, j){
+  let node
+
+  while(i < j){
+    switch(src[i]){
+      case '<':
+        node = {}
+
+    }
+
   }
-};
+}
+
+function toHTML(page){
+  let output = "<!DOCTYPE html>";
+  let typeStack = []
+  const tokens = lex(page)
+  const token_len = tokens.length
+  output = parse(tokens, 0, token_len-1)
+  return output;
+}
+
+
+/*
+// SAVE FOR LATER
+// get source and remove hton.js script tag
+var source = document.documentElement.outerHTML
+const sIndex1 = source.lastIndexOf('<script src="hton.js">')
+const sIndex2 = source.lastIndexOf("<script src='hton.js'>") 
+if(sIndex1 == -1 && sIndex2 == -1){
+  do_nothing = true
+}
+else if( sIndex1 > sIndex2 ){
+  const source = slice(0, sIndex1)
+}
+else if( sIndex1 < sIndex2 ){
+  const source = slice(0, sIndex2)
+}
+
+// convert HTON to HTML
+document.documentElement.outerHTML = toHTML(source)
+*/
+
+// TEST DATA
+let source = `// test
+<html lang:"en" [
+  <head><body "Hello World">
+]>`
+var i
+var out = ''
+for(i of lex(source)){
+  out += "\n"+i
+}
+alert(out)
